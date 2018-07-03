f9a97193aec993250c85a0c2cb6c5cc87079e30d
Melissa Perez <you@example.com>
2018-07-03 00:14:18 -0700

    ch 2p 3

---
+++ CH2/PP_2_3/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    double dollarAmount=0.00;
+    int numberOfBars=0;
+    int numberOfCoupons=0;
+    const int COUPONS_NEEDED=7;
+
+    cout << "Enter the amount of money you have: $ ";
+    cin>>dollarAmount;
+
+    while(dollarAmount >0.00 || numberOfCoupons>=7){
+        dollarAmount--;
+        numberOfCoupons++;
+        numberOfBars++;
+        if(numberOfCoupons>=COUPONS_NEEDED){
+            numberOfCoupons-=COUPONS_NEEDED;
+            numberOfBars++;
+            numberOfCoupons++;
+        }
+    }
+     cout<<"The total number of chocolate bar(s) is: "<<numberOfBars<<"."<<endl;
+     cout<<"You have "<<numberOfCoupons<<" coupon(s)left over."<<endl;
+
+
+    return 0;
+}
