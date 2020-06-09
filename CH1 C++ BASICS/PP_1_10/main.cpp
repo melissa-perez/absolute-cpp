c478c62b2b6eed1a7f4799de7767647fb6225b4f
Melissa Perez <melissa-perez@users.noreply.github.com>
2020-06-09 14:54:10 -0400

    Update main.cpp

--- CH1 C++ BASICS/PP_1_10/main.cpp
+++ CH1 C++ BASICS/PP_1_10/main.cpp
@@ -16,21 +16,17 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    double time=0.00; //in seconds
-    const double acceleration = 32.0; //feet per second
+    double time = 0.00; //in seconds
+    const double ACCELERATION = 32.0; //feet per second
     double distance=0.00;
 
 
-    cout<<"Enter time in seconds: ";
-    cin>>time;
-    distance=(0.5)*acceleration*pow(time,2.00);
+    cout << "Enter time in seconds: ";
+    cin >> time;
+    distance = (0.5) * ACCELERATION * pow(time,2.00);
 
-    cout<<"An object in freefall for "<< time <<" secs will go a distance of "<< distance
-       <<" ft. \n";
-
-
-
-
+    cout << "An object in freefall for " << time <<" secs will go a distance of " << distance
+       << " ft. \n";
 
     return 0;
 }
