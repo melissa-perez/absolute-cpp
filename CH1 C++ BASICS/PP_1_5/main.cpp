1751f47a2b7f7a07c25e1a3bfd7d3e932314fffd
Melissa Perez <melissa-perez@users.noreply.github.com>
2020-06-09 15:00:33 -0400

    Update main.cpp

--- CH1 C++ BASICS/PP_1_5/main.cpp
+++ CH1 C++ BASICS/PP_1_5/main.cpp
@@ -4,34 +4,31 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int max_room_cap=0;
-    int number_of_people=0;
+    int max_room_cap = 0;
+    int number_of_people = 0;
 
-    cout<<"What is the maximum room capacity?\n";
-    cin>>max_room_cap;
-    cout<<"How many people are attending?"<<endl;
-    cin>>number_of_people;
+    cout << "What is the maximum room capacity?\n";
+    cin >> max_room_cap;
+    cout << "How many people are attending?" << endl;
+    cin >> number_of_people;
 
-    int diff =max_room_cap-number_of_people;
+    int diff = max_room_cap - number_of_people;
 
-    if(number_of_people<max_room_cap){
-        cout<<"It is LEGAL to hold the meeting per fire law regulations."<<endl;
-        if(diff==1){
-         cout<<"You are allowed "<<diff<< " additional person."<<endl;
-        }
-        else{
-        cout<<"You are allowed "<<diff<< " additional people."<<endl;
-        }
+    if (number_of_people < max_room_cap){
+        cout << "It is LEGAL to hold the meeting per fire law regulations." << endl;
+        
+        if(diff == 1)
+            cout << "You are allowed " << diff << " additional person." << endl;
+        else
+            cout << "You are allowed " << diff << " additional people." << endl;
     }
     else{
-
-        cout<<"It is not LEGAL to hold the meeting per fire law regulations."<<endl;
-
-        if(-1*diff==1){
-         cout<<"You must exclude "<<-1*diff<< " person."<<endl;
-        }
-        else{
-        cout<<"You must exclude "<< -1*diff<< " people."<<endl;
+        cout << "It is not LEGAL to hold the meeting per fire law regulations." << endl;
+        
+        if (-1 * diff == 1)
+           cout << "You must exclude "<< -1 * diff << " person." << endl;
+        else
+           cout << "You must exclude " << -1 * diff << " people."<<endl;
     }
-    }
+    return 0;
 }
