4ce93810a4c255c05179591de7b7c8b9ae1da6a1
Melissa Perez <melissa-perez@users.noreply.github.com>
2020-06-09 14:52:16 -0400

    Update main.cpp

--- CH1 C++ BASICS/PP_1_1/main.cpp
+++ CH1 C++ BASICS/PP_1_1/main.cpp
@@ -10,15 +10,15 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    const double METRIC_TON=35273.92;
-    double weight_cereal_box=0.0;
+    const double METRIC_TON = 35273.92;
+    double weight_cereal_box = 0.0;
 
-    cout<<"Please enter the weight of the cereal box in ounces: ";
-    cin>>weight_cereal_box;
-    cout<<"\nThe weight of the cereal box in metric tons is: "<<
-          weight_cereal_box/METRIC_TON<<" ton(s).";
-    cout<<"\nYou need " <<ceil(METRIC_TON/weight_cereal_box)<<" boxes to yield a "
-       <<"metric ton."<<endl;
+    cout << "Please enter the weight of the cereal box in ounces: ";
+    cin >> weight_cereal_box;
+    cout << "\nThe weight of the cereal box in metric tons is: " <<
+          weight_cereal_box / METRIC_TON << " ton(s).";
+    cout << "\nYou need " <<ceil(METRIC_TON/weight_cereal_box) << " boxes to yield a "
+        << "metric ton." << endl;
     return 0;
 }
 
