1e506878ac0a272799c5253eddb6c0f5b10494da
melissa perez <perezmelissa053@gmail.com>
2020-06-09 14:42:19 -0400

    minor change in main

---
+++ CH9 STRINGS/PP_9_1/main.cpp
@@ -0,0 +1,32 @@
+//Assume sentence ends with a period
+//Line break is treated as a space aka, more than
+//one "space"needs to be compressed
+//First letter should be capitalized
+//Everyhthing else is lower case
+
+#include <iostream>
+#include <cstring>
+
+
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+void stdLetters(char[]);
+
+int main(int argc, char *argv[])
+{
+    char sentence[MAX_SIZE];
+    int index = 0;
+
+    cout << "Enter a sentence up to 100 characters long: ";
+    cin.getline(sentence, MAX_SIZE);
+
+    while(sentence[index] != '\0'){
+        cout << sentence[index];
+        index++;
+    }
+
+    cout << endl;
+    return 0;
+}
