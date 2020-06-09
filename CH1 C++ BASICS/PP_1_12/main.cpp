8a5095fda95e132f6db2667422d7f1fdc2e61a8c
Melissa Perez <melissa-perez@users.noreply.github.com>
2020-06-09 15:05:47 -0400

    Update main.cpp

--- CH1 C++ BASICS/PP_1_12/main.cpp
+++ CH1 C++ BASICS/PP_1_12/main.cpp
@@ -6,25 +6,25 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     double height = 5; //assume person is at least 5 feet
-    double inches= 0.00;
-    double body_weight=0.00;
+    double inches = 0.00;
+    double body_weight = 0.00;
 
-    cout<<"Enter your height in ft: ";
-    cin>>height;
-    cout<<endl<<"Enter any additional inches: ";
-    cin>>inches;
+    cout << "Enter your height in ft: ";
+    cin >> height;
+    cout << endl << "Enter any additional inches: ";
+    cin >> inches;
 
-    if(floor(height)>5){
-        height-=5;
-        height*=60; //12 inches in a foot times 5
-        body_weight+=110;
-        body_weight+=inches*5;
-        body_weight+=height;
+    if (floor(height) > 5){
+        height -= 5;
+        height *= 60; //12 inches in a foot times 5
+        body_weight += 110;
+        body_weight += inches * 5;
+        body_weight += height;
     }
     else{
-        body_weight+=110;
-        body_weight+=inches*5;
+        body_weight += 110;
+        body_weight += inches * 5;
 }
-    cout<<"Your ideal body weight is: "<< body_weight<< " lbs."<<endl;
+    cout << "Your ideal body weight is: " << body_weight << " lbs." << endl;
     return 0;
 }
